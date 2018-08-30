64b2bb7ec3f92a3028299355f5fc79191361b8bb
Artem Yankov <yankovai3@gmail.com>
2018-08-30 12:18:52 -0700

    mnist est w custom hooks

--- mnist_estimator_data_parallel.py
+++ mnist_estimator_data_parallel.py
@@ -1,5 +1,6 @@
 import os
 import tensorflow as tf
+import numpy as np
 from clusterone import get_data_path, get_logs_path
 
 flags = tf.app.flags
@@ -7,6 +8,7 @@ flags = tf.app.flags
 #
 # Snippet for distributed learning
 #
+tf.logging.set_verbosity(tf.logging.INFO)
 try:
     config = os.environ['TF_CONFIG']
     config = json.loads(config)
@@ -20,9 +22,6 @@ try:
     os.environ['TF_CONFIG'] = json.dumps(config)
 except:
     pass
-#
-#
-#
 
 flags.DEFINE_string("log_dir",
                     get_logs_path(
@@ -35,117 +34,158 @@ flags.DEFINE_string("log_dir",
                     "/data/ as root path when running on ClusterOne cloud.")
 FLAGS = flags.FLAGS
 
-def make_model(features, labels, mode, params=None, config=None):
-    #
-    # Keras model
-    #
-    model_inp = tf.keras.layers.Input(shape=(28, 28))
+def make_model():
+    model_inp = tf.keras.layers.Input(shape=(28, 28,), name='input')
     x = tf.keras.layers.Flatten()(model_inp)
     x = tf.keras.layers.Dense(128, activation='relu')(x)
     x = tf.keras.layers.Dense(128, activation='relu')(x)
-    model_out = tf.keras.layers.Dense(10, activation=None)(x)
+    model_out = tf.keras.layers.Dense(10, activation='softmax', name='output')(x)
     model = tf.keras.models.Model(model_inp, model_out)
 
-    logits = model(features)
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(),
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    return model
 
-    # Compute predictions.
-    predicted_classes = tf.argmax(logits, 1)
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        predictions = {
-            'class': predicted_classes,
-            'prob': logits
-        }
-        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
-    # Compute loss.
-    loss = tf.losses.softmax_cross_entropy(labels, logits)
-    # Create training op with exponentially decaying learning rate.
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        global_step = tf.train.get_global_step()
-        learning_rate = tf.train.exponential_decay(
-            learning_rate=0.1,
-            global_step=global_step,
-            decay_steps=100,
-            decay_rate=0.001
+class IteratorInitializerHook(tf.train.SessionRunHook):
+    def __init__(self):
+        super(IteratorInitializerHook, self).__init__()
+        self.iterator_initializer_func = None
+
+    def after_create_session(self, session, coord):
+        self.iterator_initializer_func(session)
+
+def get_inputs(x, y, batch_size=64, shuffle=True):
+    """
+    """
+    iterator_initializer_hook = IteratorInitializerHook()
+
+    def inputs():
+        """
+        """
+        x_placeholder = tf.placeholder(tf.float32, x.shape)
+        y_placeholder = tf.placeholder(tf.float32, y.shape)
+        data_x = tf.data.Dataset.from_tensor_slices(x_placeholder)
+        data_y = tf.data.Dataset.from_tensor_slices(y_placeholder)
+        data = tf.data.Dataset.zip((data_x, data_y))
+        data = data.batch(batch_size)
+
+        if shuffle:
+            data = data.shuffle(buffer_size=batch_size * 10)
+
+        iterator = data.make_initializable_iterator()
+        next_example, next_label = iterator.get_next()
+
+        # Set runhook to initialize iterator
+        iterator_initializer_hook.iterator_initializer_func = lambda sess: sess.run(
+            iterator.initializer,
+            feed_dict={x_placeholder: x, y_placeholder: y}
         )
-        optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
-        #
-        tf.summary.scalar('optim_learning_rate', learning_rate)
 
-        train_op = optimizer.minimize(loss, global_step=global_step)
-        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+        return next_example, next_label
 
-    # Compute evaluation metrics.
-    if mode == tf.estimator.ModeKeys.EVAL:
-        eval_metric_ops = {
-            'accuracy': tf.metrics.accuracy(
-                labels=tf.argmax(labels, axis=1),
-                predictions=predicted_classes
-            )
-        }
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    return inputs, iterator_initializer_hook
 
-    return None
 
-def make_data(num_epochs=None, shuffle=True, batch_size=32):
+#def make_custom_model(features, labels, mode, params=None, config=None):
+#    #
+#    # Keras model
+#    #
+#    model_inp = tf.keras.layers.Input(shape=(28, 28))
+#    x = tf.keras.layers.Flatten()(model_inp)
+#    x = tf.keras.layers.Dense(128, activation='relu')(x)
+#    x = tf.keras.layers.Dense(128, activation='relu')(x)
+#    model_out = tf.keras.layers.Dense(10, activation=None)(x)
+#    model = tf.keras.models.Model(model_inp, model_out)
+#
+#    logits = model(features)
+#
+#    # Compute predictions.
+#    predicted_classes = tf.argmax(logits, 1)
+#    if mode == tf.estimator.ModeKeys.PREDICT:
+#        predictions = {
+#            'class': predicted_classes,
+#            'prob': logits
+#        }
+#        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+#
+#    # Compute loss.
+#    loss = tf.losses.softmax_cross_entropy(labels, logits)
+#    # Create training op with exponentially decaying learning rate.
+#    if mode == tf.estimator.ModeKeys.TRAIN:
+#        global_step = tf.train.get_global_step()
+#        learning_rate = tf.train.exponential_decay(
+#            learning_rate=0.1,
+#            global_step=global_step,
+#            decay_steps=100,
+#            decay_rate=0.001
+#        )
+#        optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
+#        tf.summary.scalar('optim_learning_rate', learning_rate)
+#
+#        train_op = optimizer.minimize(loss, global_step=global_step)
+#        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+#
+#    # Compute evaluation metrics.
+#    if mode == tf.estimator.ModeKeys.EVAL:
+#        eval_metric_ops = {
+#            'accuracy': tf.metrics.accuracy(
+#                labels=tf.argmax(labels, axis=1),
+#                predictions=predicted_classes
+#            )
+#        }
+#        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
+#
+#    return None
+
+
+def main(_):
+    model = make_model()
+
+    run_config = tf.estimator.RunConfig(
+        model_dir=FLAGS.log_dir,
+        save_summary_steps=500,
+        save_checkpoints_steps=500,
+        keep_checkpoint_max=3,
+        log_step_count_steps=50
+    )
+
+    classifier = tf.keras.estimator.model_to_estimator(
+        model,
+        config=run_config
+    )
+
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.astype('float32') / 255.0
     x_test = x_test.astype('float32') / 255.0
 
-    def _input_fn(x, y, num_epochs=num_epochs, shuffle=shuffle, batch_size=batch_size):
-        data_x = tf.data.Dataset.from_tensor_slices(x)
-        data_y = tf.data.Dataset.from_tensor_slices(y)
-        data_y = data_y.map(lambda z: tf.one_hot(z, 10, dtype=tf.int32))
-
-        data = tf.data.Dataset.zip((data_x, data_y))
-        data = data.batch(batch_size)
-        data = data.repeat(num_epochs)
-        if shuffle:
-            data = data.shuffle(buffer_size=batch_size * 10)
-
-        iterator = data.make_one_shot_iterator()
-        features, labels = iterator.get_next()
-
-        return features, labels
-
-    train_input = lambda: _input_fn(
+    train_input_fn, train_iter_hook = get_inputs(
         x_train,
-        y_train,
-        num_epochs=num_epochs,
-        batch_size=batch_size
+        tf.keras.utils.to_categorical(y_train, 10).astype(np.float32)
     )
-
-    val_input = lambda: _input_fn(
+    test_input_fn, test_iter_hook = get_inputs(
         x_test,
-        y_test,
+        tf.keras.utils.to_categorical(y_test, 10).astype(np.float32),
         shuffle=False
     )
 
     train_spec = tf.estimator.TrainSpec(
-        input_fn=train_input,
-        max_steps=1e6
+        input_fn=train_input_fn,
+        max_steps=1e4,
+        hooks=[train_iter_hook]
     )
     val_spec = tf.estimator.EvalSpec(
-        input_fn=val_input,
+        input_fn=test_input_fn,
         steps=None,
-        start_delay_secs=0,
-        throttle_secs=1
+        start_delay_secs=10,
+        throttle_secs=30,
+        hooks=[test_iter_hook]
     )
 
-    return train_spec, val_spec
-
-def main(_):
-    config = tf.estimator.RunConfig(
-        model_dir=FLAGS.log_dir,
-        save_summary_steps=500,
-        save_checkpoints_steps=5000,
-        keep_checkpoint_max=1,
-        log_step_count_steps=50
-    )
-
-    classifier = tf.estimator.Estimator(model_fn=make_model, config=config)
-    train_spec, val_spec = make_data(num_epochs=None, batch_size=64, shuffle=True)
     tf.estimator.train_and_evaluate(classifier, train_spec, val_spec)
 
 if __name__ == '__main__':
